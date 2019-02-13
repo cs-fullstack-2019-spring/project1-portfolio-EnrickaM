cee8c942a80c30be7162f6b7032dba0d9adfb7a0
EnrickaM <marshallenricka@yahoo.com>
2019-02-13 14:53:06 -0600

    progress

---
+++ index.js
@@ -0,0 +1,11 @@
+function whenYouClick(e) {
+
+
+}
+console.log("test")
+var mybutton1=document.querySelector("button");
+mybutton1.addEventListener("click",whenYouClick);
+for(var i=0;i<mybutton1.length,i++;){
+    mybuttonTag[i].addEventListener("click",WhenYouClick)
+}
+
